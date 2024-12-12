a125e72410abda3e5a64ed27520e9581b08e9b49
cmaria08 <cmaria@plazalama.com>
2024-12-12 16:23:01 -0400

    Refactor CustomersTable to enhance layout and improve readability
    - Wrapped the table in a shadowed container for better visual separation.
    - Maintained existing functionality while improving the structure of the table and its elements.
    - Ensured consistent styling and alignment for customer data display.
    - Updated button actions for better clarity and usability.

--- src/components/customers/CustomersTable.tsx
+++ src/components/customers/CustomersTable.tsx
@@ -73,51 +73,52 @@ export function CustomersTable({ customers, onUpdate }: CustomersTableProps) {
             <div className="mt-8 flow-root">
                 <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                        <table className="min-w-full divide-y divide-gray-300">
-                            <thead className="bg-gray-50">
-                                <tr>
-                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                                        Cliente
-                                    </th>
-                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Compras
-                                    </th>
-                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Última Visita
-                                    </th>
-                                    {/* <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-300">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                            Cliente
+                                        </th>
+                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                            Compras
+                                        </th>
+                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                            Última Visita
+                                        </th>
+                                        {/* <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                         Estado
                                     </th> */}
-                                    <th scope="col" className="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
-                                        Progreso
-                                    </th>
-                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                        <span className="sr-only">Acciones</span>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody className="divide-y divide-gray-200 bg-white">
-                                {customers.map((customer) => (
-                                    <tr key={customer._id}>
-                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                                            <div className="flex items-center">
-                                                <div>
-                                                    <div className="font-medium text-gray-900">
-                                                        {customer.name}
-                                                    </div>
-                                                    <div className="text-gray-500">
-                                                        {customer.email}
+                                        <th scope="col" className="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
+                                            Progreso
+                                        </th>
+                                        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                            <span className="sr-only">Acciones</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200 bg-white">
+                                    {customers.map((customer) => (
+                                        <tr key={customer._id}>
+                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <div className="font-medium text-gray-900">
+                                                            {customer.name}
+                                                        </div>
+                                                        <div className="text-gray-500">
+                                                            {customer.email}
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            <div className="text-gray-900">{customer.totalPurchases}</div>
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {formatDate(customer.updatedAt)}
-                                        </td>
-                                        {/* <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                <div className="text-gray-900">{customer.totalPurchases}</div>
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {formatDate(customer.updatedAt)}
+                                            </td>
+                                            {/* <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                             {customer.hasFreeDrink ? (
                                                 <span className="inline-flex items-center rounded-full bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800">
                                                     Bebida Gratis Disponible
@@ -128,62 +129,63 @@ export function CustomersTable({ customers, onUpdate }: CustomersTableProps) {
                                                 </span>
                                             )}
                                         </td> */}
-                                        <td className="whitespace-nowrap px-3 py-4 text-center">
-                                            <div className="flex items-center justify-center">
-                                                {customer.hasFreeDrink ? (
-                                                    <span className="inline-flex items-center rounded-full bg-green-100 px-2.5 py-0.5 text-sm font-medium text-green-800">
-                                                        ¡Bebida Gratis!
-                                                    </span>
-                                                ) : (
-                                                    <PurchaseProgress
-                                                        totalPurchases={customer.totalPurchases}
-                                                        size="md"
-                                                    />
-                                                )}
-                                            </div>
-                                        </td>
-                                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                            <div className="flex justify-end gap-2">
-                                                <Button
-                                                    variant="ghost"
-                                                    size="sm"
-                                                    onClick={() => handleAddPurchase(customer._id)}
-                                                    isLoading={loadingStates[customer._id]}
-                                                    disabled={loadingStates[customer._id]}
-                                                >
-                                                    <PlusIcon className="h-5 w-5" />
-                                                </Button>
-                                                {customer.hasFreeDrink && (
+                                            <td className="whitespace-nowrap px-3 py-4 text-center">
+                                                <div className="flex items-center justify-center">
+                                                    {customer.hasFreeDrink ? (
+                                                        <span className="inline-flex items-center rounded-full bg-green-100 px-2.5 py-0.5 text-sm font-medium text-green-800">
+                                                            ¡Bebida Gratis!
+                                                        </span>
+                                                    ) : (
+                                                        <PurchaseProgress
+                                                            totalPurchases={customer.totalPurchases}
+                                                            size="md"
+                                                        />
+                                                    )}
+                                                </div>
+                                            </td>
+                                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                <div className="flex justify-end gap-2">
                                                     <Button
                                                         variant="ghost"
                                                         size="sm"
-                                                        onClick={() => handleRedeemDrink(customer._id)}
+                                                        onClick={() => handleAddPurchase(customer._id)}
                                                         isLoading={loadingStates[customer._id]}
                                                         disabled={loadingStates[customer._id]}
                                                     >
-                                                        <GiftIcon className="h-5 w-5 text-green-600" />
+                                                        <PlusIcon className="h-5 w-5" />
                                                     </Button>
-                                                )}
-                                                <Button
-                                                    variant="ghost"
-                                                    size="sm"
-                                                    onClick={() => handleShowHistory(customer._id)}
-                                                >
-                                                    <ClockIcon className="h-5 w-5" />
-                                                </Button>
-                                                <Button
-                                                    variant="ghost"
-                                                    size="sm"
-                                                    onClick={() => handleShowQRModal(customer)}
-                                                >
-                                                    <QrCodeIcon className="h-5 w-5" />
-                                                </Button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                                    {customer.hasFreeDrink && (
+                                                        <Button
+                                                            variant="ghost"
+                                                            size="sm"
+                                                            onClick={() => handleRedeemDrink(customer._id)}
+                                                            isLoading={loadingStates[customer._id]}
+                                                            disabled={loadingStates[customer._id]}
+                                                        >
+                                                            <GiftIcon className="h-5 w-5 text-green-600" />
+                                                        </Button>
+                                                    )}
+                                                    <Button
+                                                        variant="ghost"
+                                                        size="sm"
+                                                        onClick={() => handleShowHistory(customer._id)}
+                                                    >
+                                                        <ClockIcon className="h-5 w-5" />
+                                                    </Button>
+                                                    <Button
+                                                        variant="ghost"
+                                                        size="sm"
+                                                        onClick={() => handleShowQRModal(customer)}
+                                                    >
+                                                        <QrCodeIcon className="h-5 w-5" />
+                                                    </Button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
